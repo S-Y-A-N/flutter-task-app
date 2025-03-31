4ae41faf04f4ee7806de5985978afd436f3a46b9
SYAN <syanays.git@gmail.com>
2025-04-01 02:25:05 +0300

    Add web check in loadImage

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,14 +1,17 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 double maxWidth = 1000;
 
 Widget loadImage(String path) {
-  bool imageExists = File(path).existsSync();
+  if (!kIsWeb) {
+    bool imageExists = File(path).existsSync();
 
-  if (imageExists) {
-    return Image.asset(path);
-  } else {
-    return SizedBox.shrink();
+    if (imageExists) {
+      return Image.asset(path);
+    }
   }
+
+  return SizedBox.shrink();
 }
